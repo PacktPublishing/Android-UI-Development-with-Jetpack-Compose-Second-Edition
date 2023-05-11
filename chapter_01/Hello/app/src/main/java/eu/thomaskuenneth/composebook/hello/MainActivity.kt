5dafebc10cf764a972747d6c8d32a1afd4786de1
Thomas Künneth <kuennetht@gmail.com>
2023-05-11 10:35:56 +0200

    Hello added

---
+++ chapter_01/Hello/app/src/main/java/eu/thomaskuenneth/composebook/hello/MainActivity.kt
@@ -0,0 +1,139 @@
+package eu.thomaskuenneth.composebook.hello
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Hello()
+        }
+    }
+}
+
+@Composable
+@Preview(group = "my-group-1", device = Devices.AUTOMOTIVE_1024p)
+fun GreetingWrapper() {
+    Greeting("Jetpack Compose")
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+@Composable
+@Preview(group = "my-group-1")
+fun AltGreeting(name: String = "Jetpack Compose") {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+class HelloProvider : PreviewParameterProvider<String> {
+    override val values: Sequence<String>
+        get() = listOf("PreviewParameterProvider").asSequence()
+}
+
+@Composable
+@Preview
+fun AltGreeting2(
+    @PreviewParameter(HelloProvider::class)
+    name: String
+) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+@Composable
+@Preview(locale = "de-rDE")
+fun Welcome() {
+    Text(
+        text = stringResource(id = R.string.welcome),
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+@Composable
+fun TextAndButton(name: MutableState<String>, nameEntered: MutableState<Boolean>) {
+    Row(modifier = Modifier.padding(top = 8.dp)) {
+        TextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.hint))
+            },
+            modifier = Modifier
+                .alignByBaseline()
+                .weight(1.0F),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words,
+            ),
+            keyboardActions = KeyboardActions(onAny = {
+                nameEntered.value = true
+            })
+        )
+        Button(modifier = Modifier
+            .alignByBaseline()
+            .padding(8.dp),
+            onClick = {
+                nameEntered.value = true
+            }) {
+            Text(text = stringResource(id = R.string.done))
+        }
+    }
+}
+
+@Composable
+fun Hello() {
+    val name = remember { mutableStateOf("") }
+    val nameEntered = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (nameEntered.value) {
+            Greeting(name.value)
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Welcome()
+                TextAndButton(name, nameEntered)
+            }
+        }
+    }
+}
