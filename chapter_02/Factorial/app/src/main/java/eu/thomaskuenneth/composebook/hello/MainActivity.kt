7f67a95e9866ed158cfd5c3179e97d4ed23a42d7
Thomas Künneth <kuennetht@gmail.com>
2023-05-18 12:59:20 +0200

    Update MainActivity.kt

--- chapter_02/Factorial/app/src/main/java/eu/thomaskuenneth/composebook/hello/MainActivity.kt
+++ chapter_02/Factorial/app/src/main/java/eu/thomaskuenneth/composebook/hello/MainActivity.kt
@@ -3,9 +3,12 @@ package eu.thomaskuenneth.composebook.hello
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,4 +69,41 @@ fun factorialAsString(n: Int): String {
         result *= i
     }
     return "$n! = $result"
+}
+
+@Composable
+@Preview
+fun ButtonDemo(enabled: Boolean = true) {
+    Box {
+        Button(
+            onClick = {
+                println("clicked")
+            },
+            enabled = enabled
+        ) {
+            Text("Click me!")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun BoxDemo() {
+    Box(contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .size(width = 100.dp, height = 100.dp)
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(width = 80.dp, height = 80.dp)
+                .background(Color.Yellow)
+        )
+        Text(
+            text = "Hello",
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+    }
 }
