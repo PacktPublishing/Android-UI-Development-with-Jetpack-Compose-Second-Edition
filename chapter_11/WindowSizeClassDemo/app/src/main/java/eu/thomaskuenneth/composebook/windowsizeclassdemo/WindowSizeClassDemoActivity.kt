bfd42925d093d6c6fb27772eab239edbbb286a50
Thomas Künneth <kuennetht@gmail.com>
2023-08-15 20:04:24 +0200

    Content added

--- chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
+++ chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
@@ -1,13 +1,97 @@
 package eu.thomaskuenneth.composebook.windowsizeclassdemo
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 class WindowSizeClassDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme(
+                colorScheme = defaultColorScheme()
+            ) {
+                WindowSizeClassDemoScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WindowSizeClassDemoScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = {
+                Text(text = stringResource(id = R.string.app_name))
+            })
+        }
+    ) {
+        NamesList(paddingValues = it)
+    }
+}
+
+@Composable
+fun NamesList(paddingValues: PaddingValues) {
+    LazyColumn(
+        modifier = Modifier.padding(paddingValues = paddingValues),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items((1..100).toList()) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp, horizontal = 16.dp)
+                    .border(width = 1.dp, color = MaterialTheme.colorScheme.primary),
+                text = it.toString(),
+                style = MaterialTheme.typography.displayLarge,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun defaultColorScheme() = with(isSystemInDarkTheme()) {
+    val hasDynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val context = LocalContext.current
+    when (this) {
+        true -> if (hasDynamicColor) {
+            dynamicDarkColorScheme(context)
+        } else {
+            darkColorScheme()
+        }
+
+        false -> if (hasDynamicColor) {
+            dynamicLightColorScheme(context)
+        } else {
+            lightColorScheme()
         }
     }
 }
