30795a365be4b225c1b4c536e9e961a02487205d
Thomas Künneth <kuennetht@gmail.com>
2023-08-15 13:53:33 +0200

    Project WindowSizeClassDemo added

---
+++ chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
@@ -0,0 +1,13 @@
+package eu.thomaskuenneth.composebook.windowsizeclassdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class WindowSizeClassDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
