b42c1e260249294c62dbb4ff35b56876f0c2843c
Thomas Künneth <kuennetht@gmail.com>
2023-08-16 16:34:22 +0200

    App bar changes color upon scrolling

--- chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
+++ chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
@@ -18,12 +18,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,12 +47,18 @@ class WindowSizeClassDemoActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WindowSizeClassDemoScreen() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(title = {
-                Text(text = stringResource(id = R.string.app_name))
-            })
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                scrollBehavior = scrollBehavior
+            )
         }
     ) {
         NamesList(paddingValues = it)
@@ -63,7 +71,7 @@ fun NamesList(paddingValues: PaddingValues) {
         modifier = Modifier.padding(paddingValues = paddingValues),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items((1..100).toList()) {
+        items((1..42).toList()) {
             Text(
                 modifier = Modifier
                     .fillMaxWidth()
