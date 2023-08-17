e6dd9071fd4a5312159b797ad65d57b830deb177
Thomas Künneth <kuennetht@gmail.com>
2023-08-17 13:29:37 +0200

    Support for Window Size Classes added

--- chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
+++ chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
@@ -1,5 +1,6 @@
 package eu.thomaskuenneth.composebook.windowsizeclassdemo
 
+import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +27,10 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -38,7 +46,9 @@ class WindowSizeClassDemoActivity : ComponentActivity() {
             MaterialTheme(
                 colorScheme = defaultColorScheme()
             ) {
-                WindowSizeClassDemoScreen()
+                WindowSizeClassDemoScreen(
+                    activity = this
+                )
             }
         }
     }
@@ -46,43 +56,116 @@ class WindowSizeClassDemoActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WindowSizeClassDemoScreen() {
+fun WindowSizeClassDemoScreen(activity: Activity) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
+    Scaffold(modifier = Modifier
+        .fillMaxSize()
+        .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
                 title = {
                     Text(text = stringResource(id = R.string.app_name))
-                },
-                scrollBehavior = scrollBehavior
+                }, scrollBehavior = scrollBehavior
             )
-        }
-    ) {
-        NamesList(paddingValues = it)
+        }) {
+        val list = (1..42).toList()
+//        SimpleScreen(
+//            paddingValues = it,
+//            list = list
+//        )
+        AdaptiveScreen(
+            paddingValues = it,
+            list = list,
+            activity = activity
+        )
     }
 }
 
 @Composable
-fun NamesList(paddingValues: PaddingValues) {
+fun SimpleScreen(
+    paddingValues: PaddingValues,
+    list: List<Int>
+) {
+    NumbersList(
+        paddingValues = paddingValues,
+        list = list
+    )
+}
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun AdaptiveScreen(
+    paddingValues: PaddingValues,
+    list: List<Int>,
+    activity: Activity
+) {
+    with(calculateWindowSizeClass(activity = activity)) {
+        if (widthSizeClass == WindowWidthSizeClass.Compact) {
+            NumbersList(
+                paddingValues = paddingValues,
+                list = list
+            )
+        } else {
+            NumbersGrid(
+                paddingValues = paddingValues,
+                list = list,
+                windowSizeClass = this
+            )
+        }
+    }
+}
+
+@Composable
+fun NumbersList(
+    paddingValues: PaddingValues, list: List<Int>
+) {
     LazyColumn(
         modifier = Modifier.padding(paddingValues = paddingValues),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items((1..42).toList()) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp, horizontal = 16.dp)
-                    .border(width = 1.dp, color = MaterialTheme.colorScheme.primary),
-                text = it.toString(),
-                style = MaterialTheme.typography.displayLarge,
-                textAlign = TextAlign.Center
-            )
+        items(
+            items = list
+        ) {
+            NumbersItem(it)
         }
     }
+}
+
+@Composable
+fun NumbersGrid(
+    paddingValues: PaddingValues,
+    list: List<Int>,
+    windowSizeClass: WindowSizeClass
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(
+            when (windowSizeClass.widthSizeClass) {
+                WindowWidthSizeClass.Medium -> 2
+                else -> 3
+            }
+        ),
+        modifier = Modifier.padding(paddingValues = paddingValues),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(
+            items = list
+        ) {
+            NumbersItem(number = it)
+        }
+    }
+}
+
+@Composable
+private fun NumbersItem(number: Int) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .border(width = 1.dp, color = MaterialTheme.colorScheme.primary),
+        text = number.toString(),
+        style = MaterialTheme.typography.displayLarge,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Composable
