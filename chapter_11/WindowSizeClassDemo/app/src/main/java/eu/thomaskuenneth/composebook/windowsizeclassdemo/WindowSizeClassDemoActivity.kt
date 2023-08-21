7a5e83f2fde6647dd4c1af82855e4860ddb11d8e
Thomas Künneth <kuennetht@gmail.com>
2023-08-21 10:34:10 +0200

    Foldable support added

--- chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
+++ chapter_11/WindowSizeClassDemo/app/src/main/java/eu/thomaskuenneth/composebook/windowsizeclassdemo/WindowSizeClassDemoActivity.kt
@@ -34,7 +34,9 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
@@ -42,20 +44,39 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowLayoutInfo
+import kotlinx.coroutines.launch
 
 val LocalWindowSizeClass = compositionLocalOf { WindowSizeClass.calculateFromSize(DpSize.Zero) }
 
 class WindowSizeClassDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme(
-                colorScheme = defaultColorScheme()
-            ) {
-                CompositionLocalProvider(
-                    LocalWindowSizeClass provides calculateWindowSizeClass(activity = this)
-                ) {
-                    WindowSizeClassDemoScreen()
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val activity = this@WindowSizeClassDemoActivity
+                setContent {
+                    MaterialTheme(
+                        colorScheme = defaultColorScheme()
+                    ) {
+                        val windowLayoutInfo by WindowInfoTracker.getOrCreate(
+                            context = activity
+                        ).windowLayoutInfo(activity = activity).collectAsState(
+                            initial = null
+                        )
+                        CompositionLocalProvider(
+                            LocalWindowSizeClass provides calculateWindowSizeClass(activity = activity)
+                        ) {
+                            WindowSizeClassDemoScreen(
+                                windowLayoutInfo = windowLayoutInfo
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -64,7 +85,9 @@ class WindowSizeClassDemoActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WindowSizeClassDemoScreen() {
+fun WindowSizeClassDemoScreen(
+    windowLayoutInfo: WindowLayoutInfo? = null
+) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(modifier = Modifier
         .fillMaxSize()
@@ -83,19 +106,18 @@ fun WindowSizeClassDemoScreen() {
 //        )
         AdaptiveScreen(
             paddingValues = it,
-            list = list
+            list = list,
+            windowLayoutInfo = windowLayoutInfo
         )
     }
 }
 
 @Composable
 fun SimpleScreen(
-    paddingValues: PaddingValues,
-    list: List<Int>
+    paddingValues: PaddingValues, list: List<Int>
 ) {
     NumbersList(
-        paddingValues = paddingValues,
-        list = list
+        paddingValues = paddingValues, list = list
     )
 }
 
@@ -149,9 +171,7 @@ fun defaultColorScheme() = with(isSystemInDarkTheme()) {
 
 @Composable
 fun NumbersGrid(
-    paddingValues: PaddingValues,
-    list: List<Int>,
-    columns: Int = 2
+    paddingValues: PaddingValues, list: List<Int>, columns: Int = 2
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(columns),
@@ -168,22 +188,30 @@ fun NumbersGrid(
 
 @Composable
 fun AdaptiveScreen(
-    paddingValues: PaddingValues,
-    list: List<Int>
+    paddingValues:
+    PaddingValues,
+    list: List<Int>,
+    windowLayoutInfo: WindowLayoutInfo? = null
 ) {
+    var numColumnsWhenExpanded = 3
+    windowLayoutInfo?.displayFeatures?.forEach { displayFeature ->
+        (displayFeature as FoldingFeature).run {
+            if (orientation == FoldingFeature.Orientation.VERTICAL) {
+                numColumnsWhenExpanded = 2
+            }
+        }
+    }
+
     with(LocalWindowSizeClass.current) {
         if (widthSizeClass == WindowWidthSizeClass.Compact) {
             NumbersList(
-                paddingValues = paddingValues,
-                list = list
+                paddingValues = paddingValues, list = list
             )
         } else {
             NumbersGrid(
-                paddingValues = paddingValues,
-                list = list,
-                columns = when (widthSizeClass) {
+                paddingValues = paddingValues, list = list, columns = when (widthSizeClass) {
                     WindowWidthSizeClass.Medium -> 2
-                    else -> 3
+                    else -> numColumnsWhenExpanded
                 }
             )
         }
